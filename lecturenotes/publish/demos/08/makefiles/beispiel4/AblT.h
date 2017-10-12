b1b994a213802ab5cd116ce59457162d55f1d53d
Bartosz Kostrzewa <bartosz_kostrzewa@fastmail.com>
2017-10-12 15:21:37 +0200

    bring up to date to final state

--- lecturenotes/publish/demos/08/makefiles/beispiel4/AblT.h
+++ lecturenotes/publish/demos/08/makefiles/beispiel4/AblT.h
@@ -1,1 +1,5 @@
+#ifndef ABLT_H
+#define ABLT_H
+#define MY_PI 3.14
 double AblT( double*, unsigned int, unsigned int );
+#endif // ifndef(ABLT_H)
--- lecturenotes/publish/demos/08/makefiles/beispiel4/kinEnerg.h
+++ lecturenotes/publish/demos/08/makefiles/beispiel4/kinEnerg.h
@@ -1,1 +1,4 @@
+#ifndef KINENERG_H
+#define KINENERG_H
 double kinEnerg(double*, double, unsigned int, unsigned int);
+#endif // ifndef(KINENERG_H)
